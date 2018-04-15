a75f3c720234b31325e846141a29251e9bfd10a6
Uncle Engineer <eng.imron@gmail.com>
2018-04-15 18:18:37 +0700

    add datetime

--- webstock.py
+++ webstock.py
@@ -15,40 +15,54 @@ def checkstock(name):
     #print(page_soup)
 
     x = page_soup.findAll('div',{'class':'col-xs-6'})
+    y = page_soup.findAll('span',{'class':'stt-remark'})
+
+    
+    
     
     #print(x)
     #print(x[2].text)
 
     price = float(x[2].text)
     sname = x[0].text
+    dttext = y[0].text
+    dtext = dttext[-19:]
 
-    return price, sname
+    return price, sname, dtext
 
 
 
 stockname = []
 stockprice = []
-
-price_ptt, name_ptt = checkstock('PTT')
+stockdate = []
+#-------------------------------------
+price_ptt, name_ptt, date_ptt = checkstock('PTT')
 stockname.append(name_ptt)
 stockprice.append(price_ptt)
+stockdate.append(date_ptt)
 
 print("{} Price: {:.2f} baht".format(name_ptt,price_ptt))
-
-price_tmb, name_tmb = checkstock('TMB')
+print("Update: {}".format(date_ptt))
+print("----------------------")
+#-------------------------------------
+price_tmb, name_tmb, date_tmb = checkstock('TMB')
 stockname.append(name_tmb)
 stockprice.append(price_tmb)
+stockdate.append(date_tmb)
 print("{} Price: {:.2f} baht".format(name_tmb,price_tmb))
-
+print("Update: {}".format(date_tmb))
+print("----------------------")
+#-------------------------------------
 def writecsv():
     filename = 'stockprice.csv'
     with open(filename, 'a' , newline='') as file:
         fw = csv.writer(file)
         
-        for s,p in zip(stockname,stockprice):
+        for s,p,sd in zip(stockname,stockprice,stockdate):
             data = []
             data.append(s)
             data.append(p)
+            data.append(sd)
             fw.writerow(data)
 
     print("CSV File saved. stockprice.csv")
